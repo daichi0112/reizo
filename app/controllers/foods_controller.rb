0bdea8c8a2ba77e64a4ff52ad26bb583fd406928
daichi0112 <d.kawakami0112@gmail.com>
2020-11-16 16:14:43 +0900

    食材登録機能実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,10 +1,24 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
   def new
+    @food = Food.new
   end
   
   def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def food_params
+    params.require(:food).permit(:name, :number, :unit_id, :bb_date, :category_id).merge(user_id: current_user.id)
   end
 end
