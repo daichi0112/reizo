a0854d72c498df8d77d0d233298e8e8c7f61cd9e
daichi0112 <d.kawakami0112@gmail.com>
2020-11-17 20:46:28 +0900

    トップページの条件分岐

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,5 @@
 class FoodsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
 
   def index
     @foods = Food.includes(:user).order("bb_date")
