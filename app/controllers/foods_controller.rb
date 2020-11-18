62eb20b5f998654d6e0019ad18eaf6bd4e5b9486
daichi0112 <d.kawakami0112@gmail.com>
2020-11-18 21:44:56 +0900

    食材編集機能実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,6 @@
 class FoodsController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_food, only: [:edit, :update]
 
   def index
     @foods = Food.includes(:user).order("bb_date")
@@ -18,9 +19,24 @@ class FoodsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @food.update(food_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def food_params
     params.require(:food).permit(:name, :number, :unit_id, :bb_date, :category_id).merge(user_id: current_user.id)
+  end
+
+  def set_food
+    @food = Food.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'foods#index'
-  resources :foods, only: [:new, :create]
+  resources :foods, only: [:new, :create, :edit, :update]
 end
