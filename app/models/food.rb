bc3d6ca1f6a73368f02a63873654802b1d851888
daichi0112 <d.kawakami0112@gmail.com>
2020-11-21 03:38:53 +0900

    foodモデルのバリデーション修正

--- app/models/food.rb
+++ app/models/food.rb
@@ -11,7 +11,8 @@ class Food < ApplicationRecord
     validates :bb_date
     validates :category
 
-    validates :number, inclusion: { in: 0..10_000, message: 'Out of setting range' }
+    validates :number, numericality: { message: 'Half-width number' },
+                       inclusion: { in: 0..10_000, message: 'Out of setting range' }
   end
 
   with_options numericality: { other_than: 1, message: 'Select' } do
