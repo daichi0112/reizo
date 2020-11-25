f2a1a5420bcc2a077d3c9afee39625836061501f
daichi0112 <d.kawakami0112@gmail.com>
2020-11-26 04:15:50 +0900

    JavaScriptによるトップページ修正

---
+++ app/javascript/decoration.js
@@ -0,0 +1,37 @@
+function decoration () {
+  const headerBtn = document.querySelectorAll('#header-btn');
+  const searchBtn = document.getElementById('search-btn');
+  const foods = document.querySelectorAll('.food');
+
+  // mouseover
+  headerBtn.forEach((header) => {
+    header.addEventListener('mouseover', () => {
+      header.setAttribute('style', 'background-color:#f3c52f;');
+    });
+  });
+  searchBtn.addEventListener('mouseover', () => {
+    searchBtn.setAttribute('style', 'background-color:#f0e0ac;');
+  });
+  foods.forEach((food) => {
+    food.addEventListener('mouseover', () => {
+      food.setAttribute('style', 'background-color:#f58e44;');
+    });
+  });
+
+  // mouseout
+  headerBtn.forEach((header) => {
+    header.addEventListener('mouseout', () => {
+      header.removeAttribute('style', 'background-color:#f3c52f;');
+    });
+  });
+  searchBtn.addEventListener('mouseout', () => {
+    searchBtn.removeAttribute('style', 'background-color:#f0e0ac;');
+  });
+  foods.forEach((food) => {
+    food.addEventListener('mouseout', () => {
+      food.removeAttribute('style', 'background-color:#f58e44;');
+    });
+  });
+}
+
+window.addEventListener('load', decoration);
