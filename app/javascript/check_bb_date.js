38ca4747b50aef1adb4e13a9bad4a89f15cd6fc2
daichi0112 <d.kawakami0112@gmail.com>
2020-11-29 03:24:47 +0900

    賞味期限チェック機能修正

--- app/javascript/check_bb_date.js
+++ app/javascript/check_bb_date.js
@@ -1,20 +1,28 @@
 function bb_date () {
   const date = new Date();
-  // 賞味期限チェックの日付設定
+  const date2 = new Date();
+
+  // 賞味期限チェックの日付設定（7日後）
   date.setDate(date.getDate() + 7);
 
   // 月日はそれぞれ二桁表示 例) 2020-01-01
   const setBbDay = date.getFullYear() + '-' + ("0" + (date.getMonth() + 1)).slice(-2) + '-' + ("0" + date.getDate()).slice(-2);
 
-  // 賞味期限7日前の食材の表示を変更
+  // 今日の日付
+  const today = date2.getFullYear() + '-' + ("0" + (date2.getMonth() + 1)).slice(-2) + '-' + ("0" + date2.getDate()).slice(-2);
+
+  // 「賞味期限7日前」「賞味期限当日〜賞味期限切れ」の食材の表示を変更
   const foodLink = document.querySelectorAll('.food-link');
   foodLink.forEach((link) => {
     const checkDate = link.querySelectorAll('.day');
     checkDate.forEach((bbDate) => {
-      if (bbDate.innerHTML <= setBbDay) {
+      if (bbDate.innerHTML <= today) {
         link.setAttribute('style', 'color:red;');
+      } else if (bbDate.innerHTML <= setBbDay) {
+        link.setAttribute('style', 'color:#ff9e0b;');
       };
     });
   });
+
 }
 window.addEventListener('load', bb_date);
