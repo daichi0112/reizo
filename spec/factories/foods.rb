c72702fcdc63dc4313c99a0481dbf6b563bf0046
daichi0112 <d.kawakami0112@gmail.com>
2020-11-30 04:10:46 +0900

    bb_dateカラムのバリデーション修正

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name { 'じゃがいも' }
     number { Faker::Number.between(from: 1, to: 10_000) }
     unit_id { Faker::Number.between(from: 2, to: 9) }
-    bb_date { Faker::Date.backward }
+    bb_date { Date.today }
     category_id { Faker::Number.between(from: 2, to: 14) }
 
     association :user
